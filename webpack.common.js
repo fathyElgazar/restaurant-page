48b1a3762d6be9b79572cadbc038bbb3cd8780ed
fathyElgazar <gazzarDev@gmail.com>
2024-03-04 22:31:03 +0200

    Fix image loader

--- webpack.common.js
+++ webpack.common.js
@@ -16,6 +16,14 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'images', // output directory for images in dist folder
+        },
+      },
     ],
   },
   output: {
